339325f3d6a545ceafb2dfa796f54e6bef650256
jackson <lealjackson@hotmail.com>
2020-01-20 08:20:03 -0300

    arrumando o start PORT

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ var _app = _interopRequireDefault(require("./app"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 (async () => {
   try {
     const app = await (0, _app.default)();
-    const server = app.listen(port, () => console.info(`app running on port ${port}`));
+    const server = app.listen(process.env.PORT || 3000, () => console.info(`app running on port ${port}`));
     const exitSignals = ["SIGINT", "SIGTERM", "SIGQUIT"];
     exitSignals.map(sig => process.on(sig, () => server.close(err => {
       if (err) {
